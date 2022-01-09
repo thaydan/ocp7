145633859eb79d3c0b34eba1129329ef03d136bd
Romain Royer <romain.royer@outlook.com>
2022-01-09 11:56:19 +0100

    issue-1

---
+++ src/Entity/Product.php
@@ -0,0 +1,260 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $brand;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $name;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $OS;
+
+    #[ORM\Column(type: 'integer')]
+    private $storage;
+
+    #[ORM\Column(type: 'integer')]
+    private $RAM;
+
+    #[ORM\Column(type: 'float')]
+    private $screenSize;
+
+    #[ORM\Column(type: 'integer')]
+    private $weight;
+
+    #[ORM\Column(type: 'float')]
+    private $length;
+
+    #[ORM\Column(type: 'float')]
+    private $width;
+
+    #[ORM\Column(type: 'float')]
+    private $height;
+
+    #[ORM\Column(type: 'integer')]
+    private $battery;
+
+    #[ORM\Column(type: 'string', length: 10)]
+    private $connectivity;
+
+    #[ORM\Column(type: 'boolean')]
+    private $microSD;
+
+    #[ORM\Column(type: 'string', length: 10)]
+    private $color;
+
+    #[ORM\Column(type: 'datetime_immutable')]
+    private $createdAt;
+
+    #[ORM\Column(type: 'datetime_immutable')]
+    private $updatedAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getBrand(): ?string
+    {
+        return $this->brand;
+    }
+
+    public function setBrand(?string $brand): self
+    {
+        $this->brand = $brand;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getOS(): ?string
+    {
+        return $this->OS;
+    }
+
+    public function setOS(?string $OS): self
+    {
+        $this->OS = $OS;
+
+        return $this;
+    }
+
+    public function getStorage(): ?int
+    {
+        return $this->storage;
+    }
+
+    public function setStorage(int $storage): self
+    {
+        $this->storage = $storage;
+
+        return $this;
+    }
+
+    public function getRAM(): ?int
+    {
+        return $this->RAM;
+    }
+
+    public function setRAM(int $RAM): self
+    {
+        $this->RAM = $RAM;
+
+        return $this;
+    }
+
+    public function getScreenSize(): ?float
+    {
+        return $this->screenSize;
+    }
+
+    public function setScreenSize(float $screenSize): self
+    {
+        $this->screenSize = $screenSize;
+
+        return $this;
+    }
+
+    public function getWeight(): ?int
+    {
+        return $this->weight;
+    }
+
+    public function setWeight(int $weight): self
+    {
+        $this->weight = $weight;
+
+        return $this;
+    }
+
+    public function getLength(): ?float
+    {
+        return $this->length;
+    }
+
+    public function setLength(float $length): self
+    {
+        $this->length = $length;
+
+        return $this;
+    }
+
+    public function getWidth(): ?float
+    {
+        return $this->width;
+    }
+
+    public function setWidth(float $width): self
+    {
+        $this->width = $width;
+
+        return $this;
+    }
+
+    public function getHeight(): ?float
+    {
+        return $this->height;
+    }
+
+    public function setHeight(float $height): self
+    {
+        $this->height = $height;
+
+        return $this;
+    }
+
+    public function getBattery(): ?int
+    {
+        return $this->battery;
+    }
+
+    public function setBattery(int $battery): self
+    {
+        $this->battery = $battery;
+
+        return $this;
+    }
+
+    public function getConnectivity(): ?string
+    {
+        return $this->connectivity;
+    }
+
+    public function setConnectivity(string $connectivity): self
+    {
+        $this->connectivity = $connectivity;
+
+        return $this;
+    }
+
+    public function getMicroSD(): ?bool
+    {
+        return $this->microSD;
+    }
+
+    public function setMicroSD(bool $microSD): self
+    {
+        $this->microSD = $microSD;
+
+        return $this;
+    }
+
+    public function getColor(): ?string
+    {
+        return $this->color;
+    }
+
+    public function setColor(string $color): self
+    {
+        $this->color = $color;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeImmutable $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+}
