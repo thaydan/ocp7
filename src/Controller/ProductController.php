a458f046120c30057cbebe4e6eacc418fce19cfd
Romain Royer <romain.royer@outlook.com>
2022-02-07 23:32:33 +0100

    issue-8

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -8,9 +8,12 @@ use App\Repository\ProductRepository;
 use App\Service\FormPaginationHandler;
 use JMS\Serializer\SerializationContext;
 use JMS\Serializer\SerializerInterface;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use Nelmio\ApiDocBundle\Annotation\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use OpenApi\Annotations as OA;
 
 #[Route('/api/product')]
 class ProductController extends AbstractController
@@ -28,6 +31,28 @@ class ProductController extends AbstractController
      * @throws FormPaginationErrorException
      */
     #[Route('', name: 'product_index', methods: ['GET'])]
+    /**
+     * List the rewards of the specified user.
+     *
+     * This call takes into account all confirmed awards, but not pending or refused awards.
+     *
+     * @OA\Response(
+     *     response=200,
+     *     description="Returns all the product of an user",
+     *     @OA\JsonContent(
+     *        type="array",
+     *        @OA\Items(ref=@Model(type=Product::class, groups={"product:list"}))
+     *     )
+     * )
+     * @OA\Parameter(
+     *     name="order",
+     *     in="query",
+     *     description="The field used to order rewards",
+     *     @OA\Schema(type="string")
+     * )
+     * @OA\Tag(name="products")
+     * @Security(name="Bearer")
+     */
     public function index(ProductRepository $productRepository): Response
     {
         $pagination = $this->paginationHandler->handle();
